915183e7d8ad58676c002fd120f7c9b37ed85616
itzFlubby <leonardknapp@web.de>
2021-05-28 17:26:56 +0200

    Added emojis to commandModule

--- modules/data/commandModule.py
+++ modules/data/commandModule.py
@@ -1,8 +1,9 @@
 class commandModule:
-    def __init__(self, pCommandNameList, pModule, pModuleName, pEnableTrustet, pAllowedRoles):
+    def __init__(self, pCommandNameList, pModule, pModuleName, pEmoji, pEnableTrustet, pAllowedRoles):
         self.commandNameList = pCommandNameList
         self.module = pModule
         self.moduleName = pModuleName
+        self.emoji = pEmoji
         self.enableTrustet = pEnableTrustet
         self.allowedRoles = pAllowedRoles
         
